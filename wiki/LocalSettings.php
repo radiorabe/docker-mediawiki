bfa1ed668aa163066aba60ce90f885bee2762bbd
Lucas Bickel <hairmare@purplehaze.ch>
2015-12-26 12:51:09 +0100

    Setup mediawiki on wiki.cap.me with VisualEditor

---
+++ wiki/LocalSettings.php
@@ -0,0 +1,158 @@
+<?php
+# See includes/DefaultSettings.php for all configurable settings
+# and their default values, but don't forget to make changes in _this_
+# file, not there.
+#
+# Further documentation for configuration settings may be found at:
+# https://www.mediawiki.org/wiki/Manual:Configuration_settings
+
+# Protect against web entry
+if ( !defined( 'MEDIAWIKI' ) ) {
+	exit;
+}
+
+## Uncomment this to disable output compression
+# $wgDisableOutputCompression = true;
+
+$wgSitename = "DevWiki";
+$wgMetaNamespace = "Project";
+
+## The URL base path to the directory containing the wiki;
+## defaults for all runtime URL paths are based off of this.
+## For more information on customizing the URLs
+## (like /w/index.php/Page_title to /wiki/Page_title) please see:
+## https://www.mediawiki.org/wiki/Manual:Short_URL
+$wgScriptPath = "";
+$wgScriptExtension = ".php";
+
+## Path to articles is set up so that pages are reachable on /Page_Name
+$wgArticlePath = "/$1";
+
+## The protocol and server name to use in fully-qualified URLs
+$wgServer = "http://wiki.vcap.me";
+
+## The relative URL path to the skins directory
+$wgStylePath = "$wgScriptPath/skins";
+
+## The relative URL path to the logo.  Make sure you change this from the default,
+## or else you'll overwrite your logo when you upgrade!
+$wgLogo = "$wgScriptPath/resources/assets/wiki.png";
+
+## UPO means: this is also a user preference option
+
+$wgEnableEmail = true;
+$wgEnableUserEmail = true; # UPO
+
+$wgEmergencyContact = "apache@localhost";
+$wgPasswordSender = "apache@localhost";
+
+$wgEnotifUserTalk = false; # UPO
+$wgEnotifWatchlist = false; # UPO
+$wgEmailAuthentication = true;
+
+## Database settings
+$wgDBtype = "mysql";
+$wgDBserver = "mysql";
+$wgDBname = "mediawiki";
+$wgDBuser = "root";
+$wgDBpassword = "secret";
+
+# MySQL specific settings
+$wgDBprefix = "";
+
+# MySQL table options to use during installation or update
+$wgDBTableOptions = "ENGINE=InnoDB, DEFAULT CHARSET=utf8";
+
+# Experimental charset support for MySQL 5.0.
+$wgDBmysql5 = true;
+
+## Shared memory settings
+$wgMainCacheType = CACHE_NONE;
+$wgMemCachedServers = array();
+
+## To enable image uploads, make sure the 'images' directory
+## is writable, then set this to true:
+$wgEnableUploads = false;
+$wgUseImageMagick = true;
+$wgImageMagickConvertCommand = "/usr/bin/convert";
+
+# InstantCommons allows wiki to use images from http://commons.wikimedia.org
+$wgUseInstantCommons = false;
+
+## If you use ImageMagick (or any other shell command) on a
+## Linux server, this will need to be set to the name of an
+## available UTF-8 locale
+$wgShellLocale = "C.UTF-8";
+
+## If you want to use image uploads under safe mode,
+## create the directories images/archive, images/thumb and
+## images/temp, and make them all writable. Then uncomment
+## this, if it's not already uncommented:
+#$wgHashedUploadDirectory = false;
+
+## Set $wgCacheDirectory to a writable directory on the web server
+## to make your wiki go slightly faster. The directory should not
+## be publically accessible from the web.
+#$wgCacheDirectory = "$IP/cache";
+
+# Site language code, should be one of the list in ./languages/Names.php
+$wgLanguageCode = "en";
+
+$wgSecretKey = "1a0e46df5a9081c7c790dbf60588eea56c87c9634e155572cc2da879147539a3";
+
+# Site upgrade key. Must be set to a string (default provided) to turn on the
+# web installer while LocalSettings.php is in place
+$wgUpgradeKey = "b31022590a7b3b8f";
+
+## For attaching licensing metadata to pages, and displaying an
+## appropriate copyright notice / icon. GNU Free Documentation
+## License and Creative Commons licenses are supported so far.
+$wgRightsPage = ""; # Set to the title of a wiki page that describes your license/copyright
+$wgRightsUrl = "";
+$wgRightsText = "";
+$wgRightsIcon = "";
+
+# Path to the GNU diff3 utility. Used for conflict resolution.
+$wgDiff3 = "/usr/bin/diff3";
+
+## Default skin: you can change the default skin. Use the internal symbolic
+## names, ie 'vector', 'monobook':
+$wgDefaultSkin = "vector";
+
+# Enabled skins.
+# The following skins were automatically enabled:
+require_once "$IP/skins/CologneBlue/CologneBlue.php";
+require_once "$IP/skins/Modern/Modern.php";
+require_once "$IP/skins/MonoBook/MonoBook.php";
+require_once "$IP/skins/Vector/Vector.php";
+
+
+# Enabled Extensions. Most extensions are enabled by including the base extension file here
+# but check specific extension documentation for more details
+# The following extensions were automatically enabled:
+require_once "$IP/extensions/Cite/Cite.php";
+require_once "$IP/extensions/Interwiki/Interwiki.php";
+require_once "$IP/extensions/Renameuser/Renameuser.php";
+require_once "$IP/extensions/SpamBlacklist/SpamBlacklist.php";
+require_once "$IP/extensions/SyntaxHighlight_GeSHi/SyntaxHighlight_GeSHi.php";
+
+# VisualEditor Extension
+require_once "$IP/extensions/VisualEditor/VisualEditor.php";
+
+# Enable by default for everybody
+$wgDefaultUserOptions['visualeditor-enable'] = 1;
+
+# Don't allow users to disable it
+# $wgHiddenPrefs[] = 'visualeditor-enable';
+
+# OPTIONAL: Enable VisualEditor's experimental code features
+# #$wgDefaultUserOptions['visualeditor-enable-experimental'] = 1;
+
+$wgVirtualRestConfig['modules']['parsoid'] = array(
+  // URL to the Parsoid instance
+  // Use port 8142 if you use the Debian package
+  'url' => 'http://parsoid:8000',
+  'domain' => 'localhost',
+  'prefix' => 'localhost',
+);
+
