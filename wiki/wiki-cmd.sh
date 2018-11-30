35860957bf8597f40ec1ad3481a7b36e761ba3a1
Lucas Bickel <hairmare@purplehaze.ch>
2018-11-30 23:38:04 +0100

    :sparkles: support custom root ca

--- wiki/wiki-cmd.sh
+++ wiki/wiki-cmd.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+if [ -f /usr/local/share/ca-certificates/ca.crt ]; then
+    update-ca-certificates
+fi
+
 if [ ! -f ./extensions/SemanticMediaWiki/.smw.json ]; then
     php maintenance/update.php --skip-external-dependencies --quick
 fi
