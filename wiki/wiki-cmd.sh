02bd03493367906eda394b903062112e00a27c10
Lucas Bickel <lucas.bickel@adfinis.com>
2022-04-09 20:01:11 +0200

    chore: refactor to rabe ubi8-minimal baseimage

--- wiki/wiki-cmd.sh
+++ wiki/wiki-cmd.sh
@@ -9,4 +9,4 @@ if [ ! -f ./extensions/SemanticMediaWiki/.smw.json ]; then
     php maintenance/update.php --skip-external-dependencies --quick
 fi
 
-exec apache2-foreground -DFOREGROUND "$@"
+exec httpd -D FOREGROUND "$@"
